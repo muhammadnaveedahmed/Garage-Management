be9a7ca9f0d2cc3fd80cfd2be87700d09606e516
muhammadnaveedahmed <mnahmed786@gmail.com>
2022-06-01 14:39:47 +0200

    Bugs Fixed

--- Garage.Common/VehicleType.cs
+++ Garage.Common/VehicleType.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Garage.Common
-{
-    public enum VehicleType
-    {
-        Airplain,
-        Boat,
-        Bus,
-        Car,
-        Motorcycle,
-        Airplane,
-    }
-}
+﻿
